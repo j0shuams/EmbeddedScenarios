c7ab9f57a2dda6c11a0ea80d8d35621cccf9fd42
Angela Zhang <angzhang@microsoft.com>
2021-04-19 12:18:53 -0700

    NET 5 scenario

---
+++ NET5Scenario/NET5Library/ExampleClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.Devices.Geolocation;
+
+namespace NET5Library
+{
+    public class ExampleClass
+    {
+        public static async System.Threading.Tasks.Task<string> SampleMethodAsync()
+        {
+            var locator = new Geolocator();
+            var location = await locator.GetGeopositionAsync();
+            var position = location.Coordinate.Point.Position;
+            var latlong = string.Format("lat:{0}, long:{1}", position.Latitude, position.Longitude);
+            return latlong;
+        }
+    }
+}
